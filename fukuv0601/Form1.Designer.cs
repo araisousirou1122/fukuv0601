61f76f0f57a1d2c97b5d8c2ec3d326a439ea4a06
arai <130636424+araisousirou1122@users.noreply.github.com>
2023-06-01 13:24:01 +0900

    確認問題

--- fukuv0601/Form1.Designer.cs
+++ fukuv0601/Form1.Designer.cs
@@ -29,60 +29,22 @@
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
-            button4 = new Button();
             label1 = new Label();
             textBox1 = new TextBox();
             timer1 = new System.Windows.Forms.Timer(components);
+            button1 = new Button();
+            button2 = new Button();
             SuspendLayout();
-            // 
-            // button1
-            // 
-            button1.Location = new Point(117, 52);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
-            // 
-            // button2
-            // 
-            button2.Location = new Point(270, 62);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 23);
-            button2.TabIndex = 1;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            button3.Location = new Point(102, 161);
-            button3.Name = "button3";
-            button3.Size = new Size(75, 23);
-            button3.TabIndex = 2;
-            button3.Text = "button3";
-            button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            button4.Location = new Point(291, 174);
-            button4.Name = "button4";
-            button4.Size = new Size(75, 23);
-            button4.TabIndex = 3;
-            button4.Text = "button4";
-            button4.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(580, 127);
+            label1.Location = new Point(364, 159);
             label1.Name = "label1";
             label1.Size = new Size(38, 15);
             label1.TabIndex = 4;
             label1.Text = "label1";
+            label1.Click += label1_Click;
             // 
             // textBox1
             // 
@@ -94,18 +56,37 @@
             // timer1
             // 
             timer1.Enabled = true;
+            timer1.Tick += timer1_Tick;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(269, 137);
+            button1.Name = "button1";
+            button1.Size = new Size(70, 86);
+            button1.TabIndex = 6;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(431, 159);
+            button2.Name = "button2";
+            button2.Size = new Size(72, 26);
+            button2.TabIndex = 7;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(textBox1);
-            Controls.Add(label1);
-            Controls.Add(button4);
-            Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -113,13 +94,10 @@
         }
 
         #endregion
-
-        private Button button1;
-        private Button button2;
-        private Button button3;
-        private Button button4;
         private Label label1;
         private TextBox textBox1;
         private System.Windows.Forms.Timer timer1;
+        private Button button1;
+        private Button button2;
     }
 }
--- fukuv0601/Form1.cs
+++ fukuv0601/Form1.cs
@@ -2,58 +2,32 @@ namespace fukuv0601
 {
     public partial class Form1 : Form
     {
-        int vx = 20;
+        int vx = 10;
         public Form1()
         {
             InitializeComponent();
         }
 
-
-        int a = 1;
-
-        int b = 0;
-
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("konnnitiha");
-            MessageBox.Show($"konnitiha {textBox1.Text}kusoyarou");
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-            button1.Text = "konnnitiha";
-        }
-
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            // label1.Left += a;
-            // label1.Top -= b;
-            button4.Left += vx;
+            vx = -10;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            int vx = 5;
-            MessageBox.Show($"{vx}");
             vx = 10;
-            MessageBox.Show($"{vx}");
-
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void timer1_Tick(object sender, EventArgs e)
         {
-            MessageBox.Show($"{vx}");
+            label1.Left += vx;
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void label1_Click(object sender, EventArgs e)
         {
-            textBox1.Text = ("Ç®ëOÇÃê»ÇÀÇ•Ç©ÇÁ");
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            vx += 10;
-            //MessageBox.Show($"{vx}");
+            timer1.Enabled=false;
+            label1.Text = "araisoushirou";
         }
     }
 }
+
