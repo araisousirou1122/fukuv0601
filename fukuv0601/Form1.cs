b91c2b2e49c363aa7d9e1c4a6d7ed0a10b12c49a
arai <130636424+araisousirou1122@users.noreply.github.com>
2023-06-01 13:51:46 +0900

    b

--- fukuv0601/Form1.cs
+++ fukuv0601/Form1.cs
@@ -3,6 +3,7 @@ namespace fukuv0601
     public partial class Form1 : Form
     {
         int vx = 10;
+        string chr = "(>A<)";
         public Form1()
         {
             InitializeComponent();
@@ -11,21 +12,34 @@ namespace fukuv0601
         private void button1_Click(object sender, EventArgs e)
         {
             vx = -10;
+            label1.Text = "a-menn";　　　　　　　　　　　//ラベルが左に行くよ。
+            label1.ForeColor = Color.Red;　　　　　　　　　//テキストが変わるよ。色赤になるよ
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             vx = 10;
+            label1.Text = "namuamidabutu";　　　　　　//ラベルが右に行くよ。
+            label1.ForeColor = Color.Green;　　　　　//テキストが変わるよ。色緑になるよ
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             label1.Left += vx;
+            //if (
+            // {
+            //label1.Text = "バーロ";         //if文を作りたかった
+            //label1.Text = "バーロ";
+            // }
+            string t = label1.Text;
+            label1.Text = chr;
+            chr = t;
+
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
-            timer1.Enabled = false;
+            timer1.Enabled = false;　　　　　　　　　　　　　　　//timer止まるよテキスト自分の名前
             label1.Text = "araisoushirou";
         }
     }
